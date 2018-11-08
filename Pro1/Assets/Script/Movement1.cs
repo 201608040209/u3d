b7b71575bc24372b7c516f156c49632f3ff7704d
201608040209 <1035861933@qq.com>
2018-11-08 16:25:19 +0800

    u3d学习 1.模型（地图和坦克） 2.脚本的编写（1.移动 2.旋转 3.发射子弹）

---
+++ Pro1/Assets/Script/Movement1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement1 : MonoBehaviour {
+    public float MoveSpeed;
+    public float RotateSpeed;
+    private Rigidbody rb;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxis("VerticalPlayer1");
+        rb.velocity = transform.forward * v * MoveSpeed;
+        float h = Input.GetAxis("HorizontalPlayer1");
+        rb.angularVelocity = transform.up * h * RotateSpeed;
+
+    }
+}
---
+++ Pro1/Assets/Script/RayShooter1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter1 : MonoBehaviour {
+
+    private Camera camera;
+	// Use this for initialization
+	void Start () {
+        camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 point = new Vector3(camera.pixelWidth/2,camera.pixelHeight/2,0);
+            Ray ray = camera.ScreenPointToRay(point);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))
+            {
+                Debug.Log("Hit " + hit.point);
+            }
+        }
+	}
+}
